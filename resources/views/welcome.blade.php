74c68f30d0356118bdf82f18cb3ff574a70628d8
Julio Vinachi <julio899@gmail.com>
2020-01-22 01:09:59 -0400

    control

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/julio899/AgenceTest">GitHub</a>
                 </div>
             </div>
         </div>
