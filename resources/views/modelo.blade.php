76d917506efb00480b68c74b0c24497a9a82a754
Julio Vinachi <julio899@gmail.com>
2020-01-25 02:10:07 -0400

    up labels

--- resources/views/modelo.blade.php
+++ resources/views/modelo.blade.php
@@ -201,22 +201,22 @@
           <nav class="float-left">
             <ul>
               <li>
-                <a href="https://www.creative-tim.com">
+                <a href="https://www.github.com/julio899">
                   Creative Tim
                 </a>
               </li>
               <li>
-                <a href="https://creative-tim.com/presentation">
+                <a href="https://julio899.github.io/2019">
                   About Us
                 </a>
               </li>
               <li>
-                <a href="http://blog.creative-tim.com">
+                <a href="http://blog.github.com/julio899">
                   Blog
                 </a>
               </li>
               <li>
-                <a href="https://www.creative-tim.com/license">
+                <a href="https://www.github.com/julio899/license">
                   Licenses
                 </a>
               </li>
@@ -227,7 +227,7 @@
             <script>
               document.write(new Date().getFullYear())
             </script>, made with <i class="material-icons">favorite</i> by
-            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+            <a href="https://www.github.com/julio899" target="_blank">Julio Vinachi</a> for a better web.
           </div>
         </div>
       </footer>
