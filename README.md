b9a3e95e723c3933baeb476783e7b9ae297a49f4
Julio Vinachi <julio899@gmail.com>
2020-01-26 12:09:55 -0400

    Add BD Entorno Cloud Amazon and add seeder user by default

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 ## Test Agence in Laravel
 Requerimientos Base de Datos archivo `SQL Base`
 **correr en terminal**
+#### Montando la BD en Servicios Cloud de Amazon
+> mysql -h bdinstanceap.server.us-east-1.rds.amazonaws.com -u username -pAlgunaClave. < ~/Descargas/banco_de_dados.sql
+
+#### Instalacion de Dependencias
 > composer install
 
-> php artisan migrate
+#### Creacion de Estructuras para el Login
+> php artisan migrate:fresh && php artisan db:seed
 
 ![peview0](https://raw.githubusercontent.com/julio899/AgenceTest/master/public/img/preview0.png)
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // Creando Usuario Por Defecto
+    	$this->createUsers();
+    }
+
+    public function createUsers()
+    {
+        DB::table('users')->insert([
+            'name' =>  'Administrador',
+            'email' => 'julio899@gmail.com',
+            'password' => bcrypt('vinachi89'),
+            'remember_token' => Str::random(100),
+            'created_at' => now(),
+            'email_verified_at'=>now(),
+            'updated_at'=>now()
+        ]);
     }
 }
